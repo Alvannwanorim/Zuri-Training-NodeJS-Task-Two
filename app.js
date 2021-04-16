8bbb1a63f73bc94c8b6b1d1a97ec4e77e541827b
alvan nwanorim <alvannwanorim@gmail.com>
2021-04-16 23:59:01 +0100

    second commit

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-const fetchdata = () => {
-  fetch("http://jsonplaceholder.typicode.com/posts")
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error.message));
+const axios = require("axios");
+const fs = require("fs");
+
+const url = "http://jsonplaceholder.typicode.com/posts";
+
+const fetchdata = async () => {
+  try {
+    const { data } = await axios.get(url);
+
+    // Convert data to json string
+    const jsondata = JSON.stringify(data);
+
+    // Write the obtained file to require folder
+    fs.writeFile("./result/posts.json", jsondata, (error) => {
+      if (error) {
+        throw new Error(error.message);
+      }
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 fetchdata();
